358c778204a6d5500be31719a36423d90a5282ca
CarpeNecopinum <CarpeNecopinum@users.noreply.github.com>
2019-12-04 09:58:50 +0100

    Add unrolled version of Day 4

--- day4.jl
+++ day4.jl
@@ -3,7 +3,7 @@ using BenchmarkTools
 
 function part_1(range)
     digs = zero(MVector{6,Int})
-    function isvalid(i)    
+    function isvalid(i)
         digits!(digs, i)
         found_repeating = false
         for j in 2:length(digs)
@@ -15,9 +15,33 @@ function part_1(range)
     count(isvalid, range)
 end
 
+function part_1_unrolled(range)
+    count = 0
+    for i1 in 1:9
+        for i2 in i1:9
+            for i3 in i2:9
+                for i4 in i3:9
+                    for i5 in i4:9
+                        for i6 in i5:9
+                            if (i1 == i2 || i2 == i3 || i3 == i4 || i4 == i5 || i5 == i6)
+                                val = 10^5*i1 + 10^4*i2 + 10^3*i3 + 10^2*i4 + 10*i5 + i6
+                                #val = 10^5*i6 + 10^4*i5 + 10^3*i4 + 10^2*i3 + 10*i2 + i1
+                                if val in range
+                                    count += 1
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    count
+end
+
 function part_2(range)
     digs = zero(MVector{6,Int})
-    function isvalid(i)    
+    function isvalid(i)
         digits!(digs, i)
         found_repeating = false
         for j in 2:length(digs)
@@ -33,5 +57,35 @@ function part_2(range)
     count(isvalid, range)
 end
 
-@benchmark part_1(273025:767253)
-@benchmark part_2(273025:767253)
+function part_2_unrolled(range)
+    count = 0
+    for i1 in 1:9
+        for i2 in i1:9
+            for i3 in i2:9
+                for i4 in i3:9
+                    for i5 in i4:9
+                        for i6 in i5:9
+                            if (i1 == i2 || i2 == i3 || i3 == i4 || i4 == i5 || i5 == i6) &&
+                                !(i1 == i2 == i3) &&
+                                !(i2 == i3 == i4) &&
+                                !(i3 == i4 == i5) &&
+                                !(i4 == i5 == i6)
+                                val = 10^5*i1 + 10^4*i2 + 10^3*i3 + 10^2*i4 + 10*i5 + i6
+                                if val in range
+                                    count += 1
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    count
+end
+
+@btime part_1(273025:767253)
+@btime part_2(273025:767253)
+
+@btime part_1_unrolled(273025:767253)
+@btime part_2_unrolled(273025:767253)
