af107649ca6e94674f964702dec117c6d4a02203
CarpeNecopinum <CarpeNecopinum@users.noreply.github.com>
2019-12-09 22:12:20 +0100

    Update day7.jl

--- day7.jl
+++ day7.jl
@@ -1,10 +1,16 @@
 using OffsetArrays
 using BenchmarkTools
 using Combinatorics
+using Base.Threads:  @spawn, wait
+
+function Base.push!(r::Ref, x)
+    r[] = x
+    r
+end
 
 input = OffsetArray([3,8,1001,8,10,8,105,1,0,0,21,30,55,76,97,114,195,276,357,438,99999,3,9,102,3,9,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,2,9,1001,9,2,9,102,2,9,9,4,9,99,3,9,1002,9,5,9,1001,9,2,9,102,5,9,9,1001,9,4,9,4,9,99,3,9,1001,9,4,9,102,5,9,9,101,4,9,9,1002,9,4,9,4,9,99,3,9,101,2,9,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99], -1)
 
-function run_instruction!(memory::OffsetArray, iptr, inputs, output::Ref)
+function run_instruction!(memory::OffsetArray, iptr, inputs, output)
     opcode = memory[iptr]
     digs = digits(opcode; pad = 5)
     input_index = 1
@@ -25,7 +31,7 @@ function run_instruction!(memory::OffsetArray, iptr, inputs, output::Ref)
         return iptr + 2
     elseif digs[1] == 4 # output
         op1 = (digs[3] == 0) ? memory[memory[iptr+1]] : memory[iptr+1]
-        output[] = op1
+        push!(output, op1)
         return iptr + 2
     elseif digs[1] == 5 # jump if true
         op1 = (digs[3] == 0) ? memory[memory[iptr+1]] : memory[iptr+1]
@@ -52,23 +58,22 @@ function run_instruction!(memory::OffsetArray, iptr, inputs, output::Ref)
     return length(memory) + 1
 end
 
-function run_program!(program, inputs)
+function run_program!(program, inputs, outputs)
     iptr = 0
-    result = Ref(0)
     while iptr in eachindex(program)
-        iptr = run_instruction!(program, iptr, inputs, result)
+        iptr = run_instruction!(program, iptr, inputs, outputs)
     end
-    result[]
 end
 
 function test_amps(program, phase_settings)
     signal = 0
     for i in 1:5
-        signal = run_program!(copy(program), [phase_settings[i], signal])
+        output = Ref(0)
+        run_program!(copy(program), [phase_settings[i], signal], output)
+        signal = output[]
     end
     signal
 end
-
 
 function find_boostiest(program)
     maximum(permutations(0:4)) do perm
@@ -76,12 +81,25 @@ function find_boostiest(program)
     end
 end
 
-@btime find_boostiest(input) # part 1
 
+function test_loop_amps(program, phase_settings)
+    channels = ntuple(i->Channel{Int}(1), 5)
+    push!.(channels, phase_settings)
 
-# TODO: part 2
+    @spawn push!(channels[1], 0)
+    machines = ntuple(5) do i
+        @spawn run_program!(copy(program), channels[i], channels[mod1(i+1,5)])
+    end
+    wait.(machines)
 
-function test_loop_amps()
-function find_boostiest_loop(program)
-
+    popfirst!(channels[1])
 end
+
+function find_boostiest_loop(program)
+    maximum(permutations(5:9)) do perm
+        (test_loop_amps(program, perm), perm)
+    end
+end
+
+@btime find_boostiest(input) # part 1
+@btime find_boostiest_loop(input) # part 2
